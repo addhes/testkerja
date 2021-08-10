6a30317a394849dfb3a60414cf995ca23415ed79
AdeSetiawan <setiawan.ade0101@gmail.com>
2021-08-10 18:29:53 +0700

    initial project

--- listproduk.php
+++ listproduk.php
@@ -35,65 +35,31 @@
             <th>Tanggal </th>
             <th>Action</th>
         </tr>
-        
-
-
-
-
-
-
         <?php
-            include "config.php";
-            if (isset($_POST['kata_kunci'])) {
-                $kata_kunci=trim($_POST['kata_kunci']);
+        include("config.php");
+        if (isset($_POST['kata_kunci'])) {
+            $kata_kunci=trim($_POST['kata_kunci']);
                 $sql="select * from produk where nama_barang like '%".$kata_kunci."%' or kode_barang like '%".$kata_kunci."%' or jumlah_barang like '%".$kata_kunci."%' order by nama_barang asc";
-    
-            }else {
-                $sql="select * from produk order by nama_barang asc";
-            }
-    
-    
-            $hasil=mysqli_query($db,$sql);
+        } else {
+            $sql="select * from produk order by nama_barang asc";
+        }
+        $query=mysqli_query($db, $sql);
+        if(mysqli_num_rows($query)==0){
+            echo "<td colspan=7>Data Masih Kosong</td>";
+        }else{
             $no=1;
-            while ($data = mysqli_fetch_array($hasil)) {
+            while($r=mysqli_fetch_assoc($query)){
+                echo "<tr>";
+                    echo "<td>$no</td>";
+                    echo "<td>" . $r['nama_barang'] . "</td>";
+                    echo "<td>" . $r['kode_barang'] . "</td>";
+                    echo "<td>" . $r['jumlah_barang'] . "</td>";
+                    echo "<td>" . $r['tanggal'] . "</td>";
+                    echo '<td><a href="edit.php?id='.$r['id'].'">Edit</a> | <a href="delete.php?id='.$r['id'].'" onclick="return confirm(\'Yakin Akan dihapus?\')">Hapus</a></td>';
+                echo "</tr>";
                 $no++;
-    
-                ?>
-                <tbody>
-                <tr>
-                    <td><?php echo $no;?></td>
-                    <td><?php echo $data["nama_barang"]; ?></td>
-                    <td><?php echo $data["kode_barang"];   ?></td>
-                    <td><?php echo $data["jumlah_barang"];   ?></td>
-                    <td><?php echo $data["tanggal"];   ?></td>
-                    <td><a href="edit.php?id='.$r['id'].'">Edit</a> | <a href="delete.php?id='.$r['id'].'" onclick="return confirm('Yakin Akan dihapus?')">Hapus</a></td>;
-                </tr>
-                </tbody>
-                <?php
             }
-
-
-
-
-//         require_once("./config.php");
-        
-//         $query=mysqli_query($db, $sql);
-//         if(mysqli_num_rows($query)==0){
-//             echo "<td colspan=7>Data Masih Kosong</td>";
-//         }else{
-//             $no=1;
-//             while($r=mysqli_fetch_assoc($query)){
-//                 echo "<tr>";
-//                     echo "<td>$no</td>";
-//                     echo "<td>" . $r['nama_barang'] . "</td>";
-//                     echo "<td>" . $r['kode_barang'] . "</td>";
-//                     echo "<td>" . $r['jumlah_barang'] . "</td>";
-//                     echo "<td>" . $r['tanggal'] . "</td>";
-//                     echo '<td><a href="edit.php?id='.$r['id'].'">Edit</a> | <a href="delete.php?id='.$r['id'].'" onclick="return confirm(\'Yakin Akan dihapus?\')">Hapus</a></td>';
-// echo "</tr>";
-//                 $no++;
-//             }
-//         }
+        }
         ?>
 
 
